350e3f970e16befa76e229ccf1a4320ac43097d5
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-01 09:46:17 +1000

    added nix config

---
+++ nixos-system/configuration.nix
@@ -0,0 +1,200 @@
+q# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      # <home-manager/nixos>
+    ];
+  fonts.fontDir.enable = true;
+  fonts.enableDefaultFonts = true;
+  fonts.fonts = with pkgs; [
+    nerdfonts
+  ];
+
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.timeout = 1;
+
+  networking.hostName = "nix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Brisbane";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.utf8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
+  services.xserver.windowManager.i3.enable = true;
+
+  programs.dconf.enable = true;
+
+  services.flatpak.enable = true;
+
+  programs.fish.enable = true;
+
+  program.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    ohMyZsh = {
+      enable = true;
+    };
+  };
+  programs.starship.enable = true;
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "au";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.noah = {
+    isNormalUser = true;
+    description = "noah";
+    initialPassword = "temp";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      librewolf
+      kate
+      # pulseaudio
+      vscode
+      jetbrains.pycharm-community
+      jetbrains.idea-community
+      autotiling
+      github-desktop
+      (import <unstable> {}).go_1_19
+      starship
+      libsForQt5.kdeconnect-kde
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
+    wget
+    htop
+    curl
+    deja-dup
+    ark
+    duplicity  
+    alacritty
+    rofi
+    polybar
+    picom
+    haskellPackages.greenclip
+    playerctl
+    brightnessctl
+    pulsemixer
+    ksnip
+    # pulseaudio
+    rofi-emoji
+    acpi
+    dunst
+    nerdfonts
+    networkmanagerapplet
+    git
+    libsForQt5.kgpg
+    pinentry
+    blueman
+    feh
+    gnupg
+    nitrogen
+    pinentry
+    onlyoffice-bin
+    gnome.dconf-editor
+    gnome.nautilus
+    unzip
+    zip
+    i3lock-color
+    python311
+    glibc
+    gcc
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+    # enable = true;
+    # enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
