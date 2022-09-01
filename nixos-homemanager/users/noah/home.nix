a9ff05a0a17b358a6a40cc04dc7a6b0ceb01d4ba
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-01 17:09:38 +1000

    WIP

--- nixos-homemanager/users/noah/home.nix
+++ nixos-homemanager/users/noah/home.nix
@@ -1,18 +1,29 @@
 { config, pkgs, ... }:
 
+# let unstable = (
+#     import (fetchTarball "https://github.com/nixos/nixpkgs/tarball/master")
+#     { inherit (config.nixpkgs) config; }
+#   );
+# in
 {
+
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "noah";
   home.homeDirectory = "/home/noah";
 
-  let
-    unstable = import
-    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/master/)
-    { config = config.nixpkgs.config; }; # reuse the current configuration
-  in {
-    home.packages = [
-      pkgs.glances
+  # let unstable = (import (fetchTarball "channel:nixos-unstable") inherit (config.nixpkgs) config; );
+  #   # (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/<branch or commit>)
+  #   # # reuse the current configuration
+  #   # { config = config.nixpkgs.config; };
+  #   # (import (fetchTarball "channel:nixos-unstable") { config = config.nixpkgs.config; }; );
+  # in
+  # home.packages = let unstable = (import (fetchTarball "https://github.com/nixos/nixpkgs/tarball/master") { inherit (config.nixpkgs) config; } ); in [
+  # home.packages = let unstable = (import <nixos-unstable>); in [
+    # home.packages = let unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/master") { config = config.nixpkgs.config; }; in [
+  home.packages = [
+      { config = config.nixpkgs.config; }
+      # pkgs.glances
       pkgs.librewolf
       pkgs.kate
       # pkgs.pulseaudio
@@ -23,10 +34,11 @@
       pkgs.virt-manager
       pkgs.qbittorrent
       # pkgs.github-desktop
-      unstable.go_1_19
+      # unstable.go_1_19
+      (import <nixos-unstable> {}).go_1_19
       pkgs.starship
     ];
-  }
+  # };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
