527cbb83e2636535965133a75428a02dba71b987
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-01 18:35:40 +1000

    add xdg associations and mimetypes

--- nixos-homemanager/users/noah/home.nix
+++ nixos-homemanager/users/noah/home.nix
@@ -1,6 +1,37 @@
 { config, pkgs, ... }:
 
-let nixpkgsConfig = if config.nixpkgs.config == null then {} else config.nixpkgs.config; in
+let
+  nixpkgsConfig = if config.nixpkgs.config == null then {} else config.nixpkgs.config;
+
+  browser = ["librewolf.desktop"];
+
+  xdgAssociations = {
+    "text/html" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/ftp" = browser;
+    "x-scheme-handler/chrome" = ["com.github.Eloston.UngoogledChromium.desktop"];
+    "x-scheme-handler/about" = browser;
+    "x-scheme-handler/unknown" = browser;
+    "application/x-extension-htm" = browser;
+    "application/x-extension-html" = browser;
+    "application/x-extension-shtml" = browser;
+    "application/xhtml+xml" = browser;
+    "application/x-extension-xhtml" = browser;
+    "application/x-extension-xht" = browser;
+
+    # "text/*" = [ "emacs.desktop" ];
+    "audio/*" = ["mpv.desktop"];
+    "video/*" = ["mpv.dekstop"];
+    "image/*" = ["imv.desktop"];
+    #"text/calendar" = [ "thunderbird.desktop" ];
+    "application/json" = browser;
+    "application/pdf" = ["okularApplication_pdf.desktop"];
+    "x-scheme-handler/tg" = ["telegramdesktop.desktop"];
+    "x-scheme-handler/spotify" = ["com.spotify.Client.desktop"];
+    "x-scheme-handler/discord" = ["com.discordapp.Discord.desktop"];
+  };
+in
 {
 
   nixpkgs.config.allowUnfree = true;
@@ -9,6 +40,15 @@ let nixpkgsConfig = if config.nixpkgs.config == null then {} else config.nixpkgs
   # paths it should manage.
   home.username = "noah";
   home.homeDirectory = "/home/noah";
+
+  home.sessionVariables = {
+    BROWSER = "librewolf";
+    TERMINAL = "alacritty";
+  };
+
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.associations.added = xdgAssociations;
+  xdg.mimeApps.defaultApplications = xdgAssociations;
 
   home.packages = let unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/master") { config = nixpkgsConfig; }; in [
       pkgs.librewolf
