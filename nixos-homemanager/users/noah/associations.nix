4e8f0c01b69be63fb59bff1b66ad9c5d3b16b4ff
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-02 10:00:54 +1000

    nixos-system & nixos-homemanager: Move xdgAssociations into associations.nix and clean up configuration.nix

---
+++ nixos-homemanager/users/noah/associations.nix
@@ -0,0 +1,39 @@
+let
+  browser = ["librewolf.desktop"];
+
+  xdgAssociations = {
+    "text/html" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/ftp" = browser;
+    "x-scheme-handler/chrome" = ["com.github.Eloston.UngoogledChromium.desktop"];
+    "x-scheme-handler/about" = browser;
+    "x-scheme-handler/unknown" = browser;
+    "application/x-extension-htm" = browser;
+    "application/x-extension-html" = browser;
+    "application/x-extension-shtml" = browser;
+    "application/xhtml+xml" = browser;
+    "application/x-extension-xhtml" = browser;
+    "application/x-extension-xht" = browser;
+
+    # "text/*" = [ "emacs.desktop" ];
+    "audio/*" = ["mpv.desktop"];
+    "video/*" = ["mpv.dekstop"];
+    "image/*" = ["imv.desktop"];
+    #"text/calendar" = [ "thunderbird.desktop" ];
+    "application/json" = browser;
+    "application/pdf" = ["okularApplication_pdf.desktop"];
+    "x-scheme-handler/tg" = ["telegramdesktop.desktop"];
+    "x-scheme-handler/spotify" = ["com.spotify.Client.desktop"];
+    "x-scheme-handler/discord" = ["com.discordapp.Discord.desktop"];
+  };
+in {
+  home.sessionVariables = {
+    BROWSER = "librewolf";
+    TERMINAL = "alacritty";
+  };
+
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.associations.added = xdgAssociations;
+  xdg.mimeApps.defaultApplications = xdgAssociations;
+}
--- nixos-system/configuration.nix
+++ nixos-system/configuration.nix
@@ -6,39 +6,52 @@
 
 {
   imports =
-    [
-      ./hardware-configuration.nix # Include the results of the hardware scan.
-      # <home-manager/nixos> # Home manager
-    ];
-  fonts.fontDir.enable = true;
-  fonts.enableDefaultFonts = true;
-  fonts.fonts = with pkgs; [
-    nerdfonts
+  [
+    ./hardware-configuration.nix # Include the results of the hardware scan.
+    # <home-manager/nixos> # Home manager
   ];
 
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.timeout = 1;
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultFonts = true;
+    fonts = [
+      pkgs.nerdfonts
+    ];
+  };
 
-  networking.hostName = "nix"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Enable Grub2 bootloader.
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      efiInstallAsRemovable = true;
+    };
+    efi.efiSysMountPoint = "/boot/efi";
+    timeout = 1;
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  programs.nm-applet.enable = true;
+    hostName = "nix";
+
+    # Enable networking
+    networkmanager.enable = true;
+
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+
+  };
   
-  # Enable bluetooth
-  hardware.bluetooth.enable = true;
 
   # Set your time zone.
   time.timeZone = "Australia/Brisbane";
@@ -47,60 +60,104 @@
   i18n.defaultLocale = "en_AU.utf8";
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services = {
+    
+    # Enable gnome keyring.
+    gnome.gnome-keyring.enable = true;
 
-  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
-  services.xserver.windowManager.i3.enable = true;
+    # Enable the OpenSSH daemon.
+    # openssh.enable = true;
 
-  virtualisation.libvirtd.enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
 
-  programs.dconf.enable = true;
+    # Enable flatpak.
+    flatpak.enable = true;
 
-  services.flatpak.enable = true;
-  programs.kdeconnect.enable = true;
-
-  programs.fish.enable = true;
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    ohMyZsh = {
+    xserver = {
+      
       enable = true;
+
+      # Configure keymap in X11
+      layout = "au";
+      xkbVariant = "";
+      
+      # Enable the KDE Plasma Desktop Environment.
+      displayManager = {
+        sddm.enable = true;
+      };
+      desktopManager = {
+        plasma5.enable = true;
+      };
+
+      # Enable the i3-gaps window manager.
+      windowManager = {
+        i3 = {
+          package = pkgs.i3-gaps;
+          enable = true;
+        };
+      };
     };
-  };
-  programs.starship.enable = true;
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "au";
-    xkbVariant = "";
+
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+
+  programs = {
+
+    nm-applet.enable = true;
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+    gnupg.agent = {
+      enable = true;
+      pinentryFlavor = "qt";
+      # enableSSHSupport = true;
+    };
+
+    dconf.enable = true;
+
+    kdeconnect.enable = true;
+
+    fish.enable = true;
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      ohMyZsh = {
+        enable = true;
+      };
+    };
+    starship.enable = true;
+
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  # Enable bluetooth.
+  hardware.bluetooth.enable = true;
+
+  # Enable virtualisation.
+  virtualisation.libvirtd.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -159,27 +216,6 @@
     pkgs.gcc
     pkgs.notepadqq
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryFlavor = "qt";
-    # enableSSHSupport = true;
-  };
-  services.gnome.gnome-keyring.enable = true;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
