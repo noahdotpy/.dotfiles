a6ba1d4cee503a582c9baf88820d626bcf1f5002
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-17 17:56:51 +1000

    nvim: moved packer auto-setup to nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,46 @@
+--------------
+--- PACKER ---
+--------------
+
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- stylua: ignore start
+require('packer').startup(function(use)
+  if is_bootstrap then
+    require('packer').sync()
+  end
+end)
+-- stylua: ignore end
+
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
+---------------
+--- MODULES ---
+---------------
+
 require 'settings'
 require 'mappings'
 require 'plugins'
+
