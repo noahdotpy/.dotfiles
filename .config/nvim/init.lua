c33b0d9f3d41a1c43f61fcab81ae80f8c32d4939
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-21 09:39:50 +1000

    nvim: hopefully made packer bootstrap more intuitive by not requiring settings or mappings because it broke things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,43 @@
+--------------
+--- PACKER ---
+--------------
+
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Automatically source and re-compile packer whenever you save this init.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
 ---------------
 --- MODULES ---
 ---------------
 
-require 'settings'
-require 'mappings'
+-- I should have more helpful messages for getting the config working
+-- and also maybe bring back the packer bootstrapping over here and only require settings and mappings if bootstrap is happening to fix a bunch of "value not found" because a plugin isnt installed
+
+if not is_bootstrap then
+  require 'settings'
+  require 'mappings'
+end
 require 'plugins'
 
