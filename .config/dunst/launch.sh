288d0fa57774281e570d77ee0c8f88e06d0461e6
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-20 11:15:02 +1000

    dunst: add dunst config

---
+++ .config/dunst/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+
+# Terminate already running dunst instances
+pkill dunst
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x dunst >/dev/null; do sleep 1; done
+
+# Launch dunst
+dunst -c "$HOME/.config/dunst/dunstrc" &
+
+notify-send "Low" --urgency low
+notify-send "Normal" --urgency normal
+notify-send "Critical" --urgency critical
