c52aea536467ab402125031b8f54a75ec19cda17
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-09-21 11:34:07 +1000

    nvim: add a helpful message to inform user of bootstrap & some better comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@
 --- PACKER ---
 --------------
 
+-- Install packer if not already
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -14,26 +15,18 @@ local ensure_packer = function()
 end
 
 local is_bootstrap = ensure_packer()
---
--- -- Install packer
--- local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
--- local is_bootstrap = false
--- if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
---   is_bootstrap = true
---   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
---   vim.cmd [[packadd packer.nvim]]
--- end
---
--- if is_bootstrap then
---   print '=================================='
---   print '    Plugins are being installed'
---   print '    Wait until Packer completes,'
---   print '       then restart nvim'
---   print '=================================='
---   return
--- end
 
--- Automatically source and re-compile packer whenever you save this init.lua
+-- Print a helpful message
+if is_bootstrap then
+  print '===================================='
+  print '| Packer has been installed to     |'
+  print '| your system, you can now run     |'
+  print '| :PackerSync to install plugins   |'
+  print '===================================='
+  return
+end
+
+-- Automatically source and re-compile packer whenever you save this neovim config
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
   command = 'source <afile> | PackerCompile',
