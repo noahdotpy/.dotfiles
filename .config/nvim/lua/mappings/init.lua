e0205d44866010990b97ca791b4be425121f7f9c
noahdotpy <80379684+noahdotpy@users.noreply.github.com>
2022-10-10 16:18:06 +1000

    nvim: Added hop keybindings

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -21,6 +21,16 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- vim.keymap.set({ 'n', 'v' }, '<Left>', '<Nop>', { silent = true })
 -- vim.keymap.set({ 'n', 'v' }, '<Right>', '<Nop>', { silent = true })
 
+------------------------
+--- => FILE NAVIGATION <= ---
+------------------------
+
+-- requires plugin github:phaazon/hop.nvim
+vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
+vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>", {})
+
 --------------------------
 --- => FILE CONTENT <= ---
 --------------------------
